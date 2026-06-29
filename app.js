5f2521f3e4030c8dacfb8045257f75da2adacff8
bynbdulla <b.markhoos@gmail.com>
2026-06-29 12:33:02 +0300

    built the html element, strat with js

---
+++ app.js
@@ -0,0 +1,7 @@
+const brd = document.querySelector('.board')
+const sqr = document.querySelector('.square')
+const reset = document.querySelector('.reset')
+
+brd.addEventListener('click', function(event){
+    
+})
