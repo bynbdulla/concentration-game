a6df653e9755662385806c15f6a8f45afea7a3bf
bynbdulla <b.markhoos@gmail.com>
2026-06-29 16:54:37 +0300

    click

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const brd = document.querySelector('.board')
-const sqr = document.querySelector('.square')
-const reset = document.querySelector('.reset')
+const brd = document.querySelector(".board");
+const sqr = document.querySelectorAll(".square");
+// const reset = document.querySelector(".reset");
+const animals = document.querySelectorAll(".animals");
 
-brd.addEventListener('click', function(event){
-    
-})
+let clickedSqr = ''
+let flipped = false;
+/// let clickedSqr = ''
+
+brd.addEventListener("click", function (event) {
+  let clickedSqr = event.target;
+  clickedSqr.classList.remove(".hidden");
+  console.log(clickedSqr);
+  // if (clickedSqr > 2 ){
+  //     // clickedSqr.classList.remove('.hidden')
+  // }
+  // if (clickedSqr === sqr[0] && clickedSqr === sqr[3]){
+  // }
+});
