9ad995095b54a689df44bee4610ea3878a568aa6
bynbdulla <b.markhoos@gmail.com>
2026-06-30 14:29:53 +0300

    the lives are decreasing

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const brd = document.querySelector(".board");
 const cards = document.querySelectorAll(".square");
 const animals = document.querySelectorAll(".animals");
+const live = document.querySelectorAll(".live");
 
 let flipped = [];
 
+let lives = 0;
 function checkCards(flipped) {
   let matched = false;
   console.log(flipped);
 
   if (flipped[0].classList[1] === flipped[1].classList[1]) {
     matched = true;
+    
   } else {
     setTimeout(function () {
       flipped[0].querySelector(".animals").classList.add("hidden");
       flipped[1].querySelector(".animals").classList.add("hidden");
     }, 1000);
+    live[lives].style.visibility = "hidden";
+    lives++;
   }
 }
 brd.addEventListener("click", function (event) {
@@ -30,8 +35,7 @@ brd.addEventListener("click", function (event) {
     checkCards(flipped);
     flipped = [];
   }
-
-  //   console.log(matched);
-  // if the images are matching keep them open
-  //if they mismatched
 });
+
+// if lives ===3 or lives >= 3
+// then game over - board is disabled or message says game over
