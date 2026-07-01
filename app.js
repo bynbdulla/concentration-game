ee4f1d0c9849f75b84ed24f37139c1e0060a6c02
bynbdulla <b.markhoos@gmail.com>
2026-07-01 21:07:45 +0300

    congrats message is showing

--- app.js
+++ app.js
@@ -5,33 +5,48 @@ const live = document.querySelectorAll(".live");
 const name = document.querySelector("#name");
 const restart = document.querySelector(".restart");
 const msg = document.querySelector("#message");
-console.log(cards);
+const msgMsg = document.querySelector('h4')
+// console.log(cards);
 
 msg.style.display = "none";
 let lives = 0;
 let flipped = [];
+let matchedCount = 0 
+let totalPairs = cards.length / 2
+
 
 function checkCards(flipped) {
-  let matched = false;
-  console.log(flipped);
-
-  if (flipped[0].classList[1] === flipped[1].classList[1]) {
-    matched = true;
-    flipped[0].style.backgroundColor = "#D6D6D6";
-    flipped[1].style.backgroundColor = "#D6D6D6";
-  } else {
-    name.textContent = "Ooops 🥲";
-
-    setTimeout(function () {
-      flipped[0].querySelector(".animals").classList.add("hidden");
-      flipped[1].querySelector(".animals").classList.add("hidden");
-      name.textContent = "FOCUS 👀";
-    }, 1000);
-
-    live[lives].style.visibility = "hidden";
-    lives++;
-  }
+    let matched = false;
+    console.log(flipped);
+    
+    if (flipped[0].classList[1] === flipped[1].classList[1]) {
+        matched = true;
+        flipped[0].style.backgroundColor = "#D6D6D6";
+        flipped[1].style.backgroundColor = "#D6D6D6";
+        name.textContent = 'Woow 😍'
+        matchedCount++
+        console.log(matchedCount);
+        
+        if (matchedCount === totalPairs){
+            name.textContent = ''
+            msg.style.display = 'flex'
+            msgMsg.textContent = 'Congrats 🎉🥳'
+        }
+        
+    } else {
+        name.textContent = "Ooops 🥲";
+        
+        setTimeout(function () {
+            flipped[0].querySelector(".animals").classList.add("hidden");
+            flipped[1].querySelector(".animals").classList.add("hidden");
+            // name.textContent = "FOCUS 👀";
+        }, 500);
+        
+        live[lives].style.visibility = "hidden";
+        lives++;
+    }
 }
+
 brd.addEventListener("click", function (event) {
   let clickedSqr = event.target;
   //   emojiH.classList.remove('disable')
@@ -45,7 +60,7 @@ brd.addEventListener("click", function (event) {
     checkCards(flipped);
     flipped = [];
   }
-  if (lives === 5) {
+  if (lives === 7) {
     setTimeout(function () {
       name.textContent = "";
     }, 1000);
@@ -60,20 +75,14 @@ restart.addEventListener("click", function () {
   flipped = [];
   brd.classList.remove("disable");
   name.textContent = "Flip 2 cards and find pairs";
+
   live.forEach(function (chance) {
     chance.style.visibility = "visible";
   });
-  cards.forEach(function(sqr){
-    sqr.querySelector('.animals').classList.add("hidden")
-    sqr.style.backgroundColor = ''
-  })
+
+  cards.forEach(function (sqr) {
+    sqr.querySelector(".animals").classList.add("hidden");
+    sqr.style.backgroundColor = "";
+  });
+  matchedCount = 0
 });
-// sqr.forEach(function (square) {
-//     square.textContent = "";
-//     square.classList.remove("disable");
-//   });
-//   //   plX = true;
-//   //   plO = false;
-//   msg.textContent = "Player X, it's your turn";
-//   brd.classList.remove("disable");
-//   plX = true;
